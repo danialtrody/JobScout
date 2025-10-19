979ed7d099699b0cfdf41a3c44e0356f86448085
danial <trody2001@gmail.com>
2025-10-19 15:57:36 +0300

    TEST

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,34 @@
-import express from 'express';
-import cors from 'cors';
-import jobRoutes from "./routes/job.route.js";
-import rateLimit from 'express-rate-limit';
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
+import express from "express";
+import cors from "cors";
+import jobRoutes from "./routes/job.route.js";
+import rateLimit from "express-rate-limit";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
-
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+// הגשת קבצי React
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000, // דקה אחת
-  max: 5, // עד 5 בקשות לדקה
+  windowMs: 60 * 1000,
+  max: 5,
 });
 app.use("/api/jobs", limiter);
-
 app.use("/api/jobs", jobRoutes);
 
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
