ecddb3caeb1d05d6c19f469eeae0b8ea51999fc5
danial <trody2001@gmail.com>
2025-10-19 18:20:30 +0300

    TEST

--- backend/server.js
+++ backend/server.js
@@ -11,28 +11,24 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Rate limiter על /api/jobs
+
+
 const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 דקה
-  max: 5,              // עד 5 בקשות בדקה
+  windowMs: 60 * 1000,
+  max: 5,
 });
 app.use("/api/jobs", limiter);
-
-// Routes
 app.use("/api/jobs", jobRoutes);
 
-// Serve React build
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,10 @@ import Card from "../components/Card";
 import Keyword from "../components/KeyWords";
 
 function HomePage() {
+  // const BASE_URL = "http://localhost:3000/api/jobs";
   const BASE_URL = "/api/jobs";
+
+  
 
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
