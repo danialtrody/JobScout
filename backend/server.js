b22eb121c24b7ea126cce854a0e65326ba2ace1c
danial <trody2001@gmail.com>
2025-10-19 15:47:36 +0300

    TEST

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,34 @@ import express from 'express';
 import cors from 'cors';
 import jobRoutes from "./routes/job.route.js";
 import rateLimit from 'express-rate-limit';
+import path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
 
-// Middleware
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
 app.use(cors());
 app.use(express.json());
 
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 דקה
+  windowMs: 60 * 1000, // דקה אחת
   max: 5, // עד 5 בקשות לדקה
 });
 app.use("/api/jobs", limiter);
 
+app.use("/api/jobs", jobRoutes);
 
-app.use("/api/jobs",jobRoutes)
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
