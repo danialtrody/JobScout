e02fcd21b3e966515614057946164eb4883e9c1f
danial <trody2001@gmail.com>
2025-10-20 19:09:20 +0300

    mobile adjusment

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -36,19 +36,19 @@ function HomePage() {
     getJobs();
   }, [position,location]);
 
-  if (loading)
+  if (!loading)
     return (
       <div className="loading-container">
         <p className="loading-title">
           🔍 Searching for the latest job positions posted in the last 24 hours...
         </p>
-        <p>
+        <p className="loading-title">
         👩‍💻 <strong>Junior</strong>, 🎓 <strong>Entry Level</strong>, and 🏢 <strong>Internship</strong> positions are included.
-        </p>
-        <p>
+        </p >
+        <p className="loading-title">
           Please be patient, it may take a few seconds to gather the most relevant opportunities.
         </p>
-        <p >
+        <p className="loading-title">
           ☕ Grab a coffee, stretch, or do a quick dance while we fetch the newest roles just for you!
         </p>
   
