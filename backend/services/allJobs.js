c3cd142612242a0d60a51071291184806676a8ed
danial <trody2001@gmail.com>
2025-11-08 17:58:31 +0200

    TEST

--- backend/services/allJobs.js
+++ backend/services/allJobs.js
@@ -177,7 +177,7 @@ async function scrollAndCollectAllJobs(page, maxJobs = 200) {
 export async function fetchAllJobs(keyword = "") {
   try {
     const { browser, page } = await connect({
-      executablePath: process.env.CHROME_PATH, // <- add this
+      executablePath: isRender ? chromium.path : process.env.CHROME_PATH,
       headless: isRender,
       args: isRender
         ? ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--disable-gpu"]
